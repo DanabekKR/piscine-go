9d0cd8cc16954a9defff61a2ad7d02f6c924739c
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-16 01:49:52 +0600

    GGGGsssAAasdsadasdfdsdfdsf

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,14 @@
 package main
 
 import "github.com/01-edu/z01"
+
 func main(){
-	for i:=0; i<26; i++{
-		z01.PrintRune(97+i)
+	for i:='a'; i<='z'; i++ {
+		
+		z01.PrintRune(i)
+
 	}
+
 	z01.PrintRune('\n')
+
 }
