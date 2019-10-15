9e5a9b80d2dd53bbbf0b2f3a1163c8e153867f9c
DanabekKR <52834222+DanabekKR@users.noreply.github.com>
2019-10-16 03:01:30 +0600

    Add files via upload

---
+++ IsNegative.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func IsNegative(n int) {
+	if (n < 0){
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
+	} else {
+		z01.PrintRune('T')
+		z01.PrintRune('\n')
+	}
+}
