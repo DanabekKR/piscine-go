f70c9f62bebae7365a1f6f7bd92137292334ed4f
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-17 04:29:42 +0600

    hope3

--- atoi.go
+++ atoi.go
@@ -4,14 +4,14 @@ func Atoi(s string) int {
 	newStr := []rune(s)
 	count := StrLen(s)
 	sstr := ""
+	if s == "+" || s == "-" || s == "" {
+		return 0
+	}
 	for i := 1; i < count; i++ {
 		sstr = sstr + string(newStr[i])
 	}
 	if newStr[0] == '-' {
 		return -1 * BasicAtoi2(sstr)
-	}
-	if s == "+" || s == "-" {
-		return 0
 	}
 	if newStr[0] == '+' {
 		return BasicAtoi2(sstr)
