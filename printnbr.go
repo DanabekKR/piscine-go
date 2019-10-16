3ce1929d243fbbb6a33e2c5aeeb886e82eef1627
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-16 22:53:05 +0600

    dsd

--- printnbr.go
+++ printnbr.go
@@ -7,6 +7,7 @@ import (
 func PrintNbr(n int) {
 	tt := 1
 	pp := 1
+	bb := 0
 	for i := 0; i < 18; i++ {
 		if n < 0 {
 			n = -1 * n
@@ -18,45 +19,79 @@ func PrintNbr(n int) {
 			break
 		}
 	}
-	if n/tt >= 10 {
-		tt = tt * 10
+
+	if pp == 18 {
+		bb = 1
 	}
-
 	for i := 1; i <= pp; i++ {
-		tt = tt / 10
-		sym := n / tt
-		n = n - sym*tt
-
-		if sym == 0 {
-			z01.PrintRune('0')
+		if bb == 1 {
+			sym := n / tt
+			n = n - sym*tt
+			if sym == 0 {
+				z01.PrintRune('0')
+			}
+			if sym == 1 {
+				z01.PrintRune('1')
+			}
+			if sym == 2 {
+				z01.PrintRune('2')
+			}
+			if sym == 3 {
+				z01.PrintRune('3')
+			}
+			if sym == 4 {
+				z01.PrintRune('4')
+			}
+			if sym == 5 {
+				z01.PrintRune('5')
+			}
+			if sym == 6 {
+				z01.PrintRune('6')
+			}
+			if sym == 7 {
+				z01.PrintRune('7')
+			}
+			if sym == 8 {
+				z01.PrintRune('8')
+			}
+			if sym == 9 {
+				z01.PrintRune('9')
+			}
+			bb = 0
+		} else {
+			tt = tt / 10
+			sym := n / tt
+			n = n - sym*tt
+			if sym == 0 {
+				z01.PrintRune('0')
+			}
+			if sym == 1 {
+				z01.PrintRune('1')
+			}
+			if sym == 2 {
+				z01.PrintRune('2')
+			}
+			if sym == 3 {
+				z01.PrintRune('3')
+			}
+			if sym == 4 {
+				z01.PrintRune('4')
+			}
+			if sym == 5 {
+				z01.PrintRune('5')
+			}
+			if sym == 6 {
+				z01.PrintRune('6')
+			}
+			if sym == 7 {
+				z01.PrintRune('7')
+			}
+			if sym == 8 {
+				z01.PrintRune('8')
+			}
+			if sym == 9 {
+				z01.PrintRune('9')
+			}
 		}
-		if sym == 1 {
-			z01.PrintRune('1')
-		}
-		if sym == 2 {
-			z01.PrintRune('2')
-		}
-		if sym == 3 {
-			z01.PrintRune('3')
-		}
-		if sym == 4 {
-			z01.PrintRune('4')
-		}
-		if sym == 5 {
-			z01.PrintRune('5')
-		}
-		if sym == 6 {
-			z01.PrintRune('6')
-		}
-		if sym == 7 {
-			z01.PrintRune('7')
-		}
-		if sym == 8 {
-			z01.PrintRune('8')
-		}
-		if sym == 9 {
-			z01.PrintRune('9')
-		}
-
 	}
 }
