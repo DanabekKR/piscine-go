437895ff86523d93b1f6bc808c5d89609a382981
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-17 04:22:28 +0600

    hope

---
+++ atoi.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Atoi(s string) int {
+	newStr := []rune(s)
+	count := StrLen(s)
+	sstr := ""
+	for i := 1; i < count; i++ {
+		sstr = sstr + string(newStr[i])
+	}
+	if newStr[0] == '-' {
+		return -1 * BasicAtoi2(sstr)
+	}
+	if newStr[0] == '+' {
+		return BasicAtoi2(sstr)
+	}
+	return BasicAtoi2(s)
+}
