85022f0bcfe85f6b3002bd90df36d60e980010dd
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-16 19:15:53 +0600

    like it

---
+++ printcombo.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb(){
+	for i := '0'; i <= '9'; i++ {
+		for j := '0';  j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i < j {
+					if j < k {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(k)
+						z01.PrintRune(44)
+						z01.PrintRune(160)
+					}
+				}				
+			}
+		}
+	} 
+}
