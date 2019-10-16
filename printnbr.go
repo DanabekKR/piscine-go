7a103affc241319dc7182118e3ac0aa9de20227b
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-16 22:30:06 +0600

    dsd

--- printnbr.go
+++ printnbr.go
@@ -1,21 +1,27 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(n int) {
 	tt := 1
 	pp := 1
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 18; i++ {
 		if n < 0 {
 			n = -1 * n
 			z01.PrintRune('-')
 		}
 		tt = 10 * tt
+		pp = i + 1
 		if n < tt {
-			pp = i + 1
 			break
 		}
 	}
+	if n/tt >= 10 {
+		tt = tt * 10
+	}
+
 	for i := 1; i <= pp; i++ {
 		tt = tt / 10
 		sym := n / tt
