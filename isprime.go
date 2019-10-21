90faa0e856672a1878eeb3207e40da472c074a25
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-21 12:11:56 +0600

    prime

---
+++ isprime.go
@@ -0,0 +1,20 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	sqVal := 0
+	for i := 0; i <= nb; i++ {
+		if i*i == nb {
+			return false
+		}
+		if i*i > nb {
+			sqVal = i - 1
+			break
+		}
+	}
+	for i := 2; i <= sqVal; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
