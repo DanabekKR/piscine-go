6af34f9976540fffbe05d7655ee5e912d6f6454c
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-22 19:24:24 +0600

    cap

--- capitalize.go
+++ capitalize.go
@@ -6,8 +6,9 @@ func Capitalize(s string) string {
 
 	if s[0] >= 'a' && s[0] <= 'z' {
 		res = res + string(rune(s[0]-32))
+	} else {
+		res = res + string(rune(s[0]))
 	}
-	res = res + string(rune(s[0]))
 	for i := 1; i < n; i++ {
 		if (s[i] >= 'a' && s[i] <= 'z') && !isLetter(rune(s[i-1])) {
 			res = res + string(rune(s[i]-32))
