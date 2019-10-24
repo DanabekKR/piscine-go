c9b37c01e209ed003b08d71efbb8c011a03dac65
DanabekKR <danabek.kaliazhdarov@gmail.com>
2019-10-25 00:30:26 +0600

    sa

---
+++ split.go
@@ -0,0 +1,37 @@
+package piscine
+
+func Split(str, charset string) []string {
+	str = str + charset
+	len := StrLen(str)
+	sepLen := StrLen(charset)
+	wCount := 0
+	for i := 0; i < len-sepLen+1; i++ {
+		tmp := ""
+		for j := 0; j < sepLen; j++ {
+			tmp = tmp + string(str[i+j])
+		}
+		if tmp == charset {
+			wCount++
+		}
+	}
+	println(wCount)
+	res := make([]string, wCount)
+	wCount = 0
+	pos := 0
+	for i := 0; i < len-sepLen+1; i++ {
+		tmp := ""
+		word := ""
+		pl := 0
+		for j := 0; j < sepLen; j++ {
+			tmp = tmp + string(str[i+j])
+			pl = j
+		}
+		if tmp == charset {
+			word = str[pos:i]
+			res[wCount] = word
+			wCount++
+			pos = i + pl + 1
+		}
+	}
+	return res
+}
